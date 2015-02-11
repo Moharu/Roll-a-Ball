30d11ee7feec616537c06bf6111e9d5cb871bd94
Joao Telo <joaotelo.nh@hotmail.com>
2015-02-11 02:49:33 -0200

    First edits, background color changes and player movement.

---
+++ Assets/Scripts/ChangeTerrain.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeTerrain : MonoBehaviour {
+
+	public bool ClickFlag;
+	// Update is called once per frame
+	void Update () {
+		if (ClickFlag == false) {
+			if (Input.GetMouseButtonDown (0)) {
+				if (renderer.material.GetColor ("_Color") == Color.red) {
+					renderer.material.SetColor ("_Color", Color.blue);
+				} else {
+					renderer.material.SetColor ("_Color", Color.red);
+				}
+				ClickFlag = true;
+			}
+		} else {
+			if (Input.GetMouseButtonUp (0)){
+				ClickFlag = false;
+			}
+		}
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
 	void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
-		rigidbody
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rigidbody.AddForce (movement*speed*Time.deltaTime); 
 	}
 }
